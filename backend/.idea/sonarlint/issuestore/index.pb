34f195ff13fff107fe1b965f6630c1501eab2f13
Jhon Serna <jhonalfaber@gmail.com>
2021-09-16 13:15:50 -0500

    Implementando el add Lista de listas

--- backend/.idea/sonarlint/issuestore/index.pb
+++ backend/.idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,3 @@
 
-o
-?src/main/java/com/sofkau/backend/web/mapper/ToDoListMapper.java,c\f\cf6e815dfe9fcef6f78dc23fbd26e735ac9d4c79
+e
+5src/main/java/com/sofkau/backend/entity/ToDoList.java,b\9\b9fa86b1a8762322353d3bb272d8fc610bbdd361
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,60 @@
-import React,{useReducer,createContext} from 'react';
+import React,{useReducer,createContext,useRef,useContext,useState} from 'react';
 
+const HOST_API = "http://localhost:8080/api";
 const initialState = {
-  toDoList: { List: [], item: {}},
-  todo: {List: [], item: {}}
+  toDoList: { list: [], item: {}},
+  toDo: {list: [], item: {}}
 }
 const Store = createContext(initialState)
 
+const FormToDoList = () => {
+  const formRef = useRef(null);
+  const {dispatch,state: {toDoList}} = useContext(Store);
+  const item = toDoList.item;
+  const [state, setState] = useState(item);
+
+  const onAdd = (event) => {
+    event.preventDefault();
+    const request = {
+      name: state.name,
+      id: null,
+      isCompleted:false
+    };
+    fetch(HOST_API + "/todoList",{
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then((toDoList) => {
+      dispatch({ type: "add-toDoList-item",item:toDoList});
+      setState({name: ""});
+      formRef.current.reset();
+    });
+  }
+
+  return <form ref={formRef}>
+    <input
+      type="text"
+      name="name"
+      placeholder="To-Do List"
+      defaultValue={item.name}
+      onChange={(event) => {
+        setState({ ...state, name: event.target.value })
+      }}  ></input>
+    {!item.id && <button onClick={onAdd}>New list</button>}
+  </form>
+}
+
+
 function reducer(state,action){
   switch (action.type) {
-    case 'prueba':
-      return state;
+    case 'add-todoList-item':
+      const todoListUp = state.toDoList.list;
+      todoListUp.push(action.item);
+      return { ...state, todoList: { list: todoListUp, item: {} } }
     default:
       return state;  
 
@@ -29,6 +74,7 @@ function App() {
     <StoreProvider>
       <style >{'body {background-color: #8f8f8f}'}</style>
         <h3 className="text-center text-white">To-DO List</h3>
+        <FormToDoList/>
 
     </StoreProvider>
   );
